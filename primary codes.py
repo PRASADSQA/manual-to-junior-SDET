dae439e17433b4bf75b79194a28d7b9e4ed10f58
Prasad Ranasinghe <janendrar@gmail.com>
2025-12-31 13:03:01 +0100

    add simple login code using python

--- primary codes.py
+++ primary codes.py
@@ -3,3 +3,12 @@ age = 23
 is_active = True
 
 print(name + "is a IT student, who is" + str(age) + "years old, and he is :"+ str(is_active))
+
+#simple login command - if/else
+user_name = str(input("Enter User name: "))
+pass_word = str(input("Enter Password: "))
+
+if user_name == "User name" and pass_word == "1234":
+    print("Login Sucessfully")
+else:
+    print("Loagin fail")
